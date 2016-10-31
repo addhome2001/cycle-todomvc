a6e1cfa15b5cc5ec03c01fd712665f5b82ee121a
Seven <seven@megalive.com.tw>
2016-10-31 22:48:23 +0800

    Modified index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { Observable } from 'rxjs';
 import Cycle from '@cycle/rxjs-run';
 import { h, makeDOMDriver} from '@cycle/dom';
-import { TodoList } from './TodoList';
-import { TodoInput } from './TodoInput';
+import TodoList from './TodoList';
+import TodoInput from './TodoInput';
 
 require('./style.scss');
 
@@ -14,13 +14,8 @@ function main({ DOM }) {
     TodoInput$, TodoList$,
     (TodoInput, TodoList) => {
       return h('div.wrapper', [
-        h('div.header', [
-          TodoInput,
-          h('button.addBtn', '+')
-        ]),
-        h('div.content', [
-          h('ul.items', TodoList)
-        ]),
+        h('div.header', TodoInput),
+        h('div.content', TodoList),
       ])
     }
   );
