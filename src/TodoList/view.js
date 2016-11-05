d6c2f0356895d67d30c2ba121f0f47ca2ef7de1a
Seven <seven@megalive.com.tw>
2016-11-05 16:19:43 +0800

    Modified css

--- src/TodoList/view.js
+++ src/TodoList/view.js
@@ -3,10 +3,11 @@ import { h } from '@cycle/dom';
 export default function view(state$) {
   return state$.map(([counter, items]) =>
     [
-      h('ul.items', items),
-      h('div.info', [
-        h('div.itemCount', [`${counter} items left`])
-      ])
+      h('ul.items', [...items,
+        h('li.info', [
+          h('div.itemCount', [`${counter} items left`])
+        ])
+      ]),
     ]
   )
 }
