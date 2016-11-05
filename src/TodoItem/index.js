fd3f7160f07d422396938dc4d3184541ac055be1
Seven <seven@megalive.com.tw>
2016-11-06 00:54:50 +0800

    Modified structure

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -4,13 +4,14 @@ import view from './view';
 
 export default function({ DOM, add$ }) {
   const { remove$, complete$ } = indent(DOM);
-  const { addItem$, counter$ } = model({ complete$, add$, remove$ });
+  const { addItem$, counter$, completeStatus$ } = model({ complete$, add$, remove$ });
   const vdom$ = view(addItem$);
 
   const sink = {
     DOM: vdom$,
     counter$,
-    remove$
+    remove$,
+    completeStatus$
   }
 
   return sink
--- src/TodoItem/model.js
+++ src/TodoItem/model.js
@@ -2,12 +2,13 @@ import { Observable } from 'rxjs';
 
 export default function({ complete$, add$, remove$ }) {
   const removeCounter$ = remove$.startWith("");
+  const completeStatus$ = complete$.startWith(false);
   const counter$ =
     complete$
       .merge(add$)
       .map(c => !c || c.length ? 1 : -1)
       .combineLatest(removeCounter$, (count, remove) => !remove ? count : (count > 0) ? -1 : 0);
-  const addItem$ = Observable.combineLatest(complete$.startWith(false), add$);
+  const addItem$ = Observable.combineLatest(completeStatus$, add$);
 
-  return { counter$, addItem$ }
+  return { counter$, addItem$, completeStatus$ }
 }
