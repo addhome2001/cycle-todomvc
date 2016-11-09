bf4930bde8eaa49c5f5b777a5e304868a4abcc27
Seven <seven@megalive.com.tw>
2016-11-09 14:21:14 +0800

    Modified structure

--- src/TodoList/model.js
+++ src/TodoList/model.js
@@ -6,14 +6,21 @@ export default function ({ items$, filterStatus$ }){
   const status$ = filterStatus$.startWith('All');
   const filter$ = status$.map(status => filterTrigger[status]);
 
+  const counter$ =
+    Collection
+      .merge(items$, item => item.counter$)
+      .scan((prev, curr) => prev + curr)
+      .startWith(0);
+
+  const itemsVdom$ =
+    Collection
+      .pluck(items$, ({ DOM, completeStatus$ }) =>
+        Observable.combineLatest(DOM, completeStatus$, filter$,
+          (DOM, completeStatus, filter) => filter(completeStatus) ? DOM : h('li', {style: {display: 'none'}})))
+
   return Observable.combineLatest(
-    Collection.merge(items$, item => item.counter$).startWith(0).scan((prev, curr) => prev + curr),
-    Collection.pluck(items$, ({ DOM, completeStatus$ }) =>
-      Observable.combineLatest(DOM, completeStatus$, filter$,
-        (DOM, completeStatus, filter) =>
-          filter(completeStatus) ? DOM : h('li', {style: {display: 'none'}})
-      ),
-    ),
+    counter$,
+    itemsVdom$,
     status$
   )
 }
