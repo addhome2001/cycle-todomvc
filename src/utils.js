5390475bab1461ad2ac59380a691e1167ac4d39a
Seven <seven@megalive.com.tw>
2017-01-16 21:19:17 +0800

    Modified api to https

--- src/utils.js
+++ src/utils.js
@@ -2,14 +2,14 @@ export const generateRandomNum = () => Math.round(Math.random() * 9) + 1;
 
 export const getUserApi = () =>
   ({
-    url: `http://jsonplaceholder.typicode.com/todos/${generateRandomNum()}`,
+    url: `https://jsonplaceholder.typicode.com/todos/${generateRandomNum()}`,
     category: 'getItems',
     method: 'GET',
   });
 
 export const setUserApi = title =>
   ({
-    url: 'http://jsonplaceholder.typicode.com/todos',
+    url: 'https://jsonplaceholder.typicode.com/todos',
     category: 'setItems',
     method: 'POST',
     send: { title },
