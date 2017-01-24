c250d897790ded49f11deaebce82189e565bf87e
Seven <seven@megalive.com.tw>
2017-01-24 14:24:33 +0800

    Added debounceTime for item request

--- src/helper.js
+++ src/helper.js
@@ -1,15 +1,9 @@
 import rxjsAdapter from '@cycle/rxjs-adapter';
 import { Observable } from 'rxjs';
 import { makeCollection } from '@cycle/collection';
-import { getUserApi, setUserApi } from './utils';
+import { getUserApi, setUserApi } from './utils/api';
 
 export const Collection = makeCollection(rxjsAdapter);
-
-export const filterTrigger = {
-  All: () => true,
-  Active: complete => !complete,
-  Completed: complete => complete,
-};
 
 export const titleResponse = response =>
   Observable
@@ -19,5 +13,7 @@ export const titleResponse = response =>
 
 export const titleRequest = inputText$ =>
   inputText$
+    // Throw away all emitted values that take less then 1s
+    .debounceTime(1000)
     .mergeMap(text => Observable.of(setUserApi(text)))
     .merge(Observable.of(getUserApi()));
